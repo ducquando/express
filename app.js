35b8957c73fddd31d4bb9128b13205c612841386
Duc-Quan Do <99700700+ducquando@users.noreply.github.com>
2023-11-17 09:41:18 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
+const { exec } = require('child_process');
 const fs = require('fs');
+
+const app = express();
 
 const port = 5001;
 
@@ -15,13 +17,21 @@ app.all('/*', function(req, res, next) {
 
 app.post('/', (req, res) => {
   const data = req.body;
-  // const fileName = data.fileName;
-  const pathToRevealApp = './presentation';
-
-  console.log(data);
-  fs.writeFile(`${pathToRevealApp}/demo.json`, JSON.stringify(data),  function (err, d) {
+  const newHTML = `<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="description"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="dist/reset.css"><link rel="stylesheet" href="dist/reveal.css"><link rel="stylesheet" href="dist/theme/white.css" id="theme"></head><body><div class="reveal"><div class="slides"><section>${data}</section></div></div><script src="dist/reveal.js"></script><script src="plugin/zoom/zoom.js"></script><script src="plugin/notes/notes.js"></script><script src="plugin/search/search.js"></script><script src="plugin/markdown/markdown.js"></script><script src="plugin/highlight/highlight.js"></script><script>Reveal.initialize({controls: true, progress: true, center: true, hash: true, plugins: [RevealZoom, RevealNotes, RevealSearch, RevealMarkdown, RevealHighlight]});</script></body>
+  </html>`;
+  console.log(data)
+  fs.writeFile(`compiler/tmp/${data.fileName}.json`, JSON.stringify(data, null, 2),  function (err, d) {
     if (err) throw err;
   });
+  const pythonProcess = exec(`python3 compiler/json_to_html.py compiler/tmp/${data.fileName}.json ../reveal.js/${data.fileName}.html`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      res.status(500).send('Error with Python Script'); 
+    }
+    console.log(`Python script output: ${stdout}`);
+    res.send(`Python script output: ${stdout}`);
+  }); 
+  
 });
 
 app.listen(port, () => {
